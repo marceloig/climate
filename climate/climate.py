197538bbc0352ead22cb1eb2e0548af6dad807d6
Igor Marcelo de Oliveira da Silva <cccmigor@gmail.com>
2018-01-16 23:05:12 -0200

    removido comentarios desnecessarios

--- climate/climate.py
+++ climate/climate.py
@@ -8,10 +8,7 @@ app.config.from_object(__name__) # load config from this file , climate.py
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'climate.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
+    DATABASE=os.path.join(app.root_path, 'climate.db')
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 FORMAT_DATE = '%Y-%m-%d'
@@ -117,26 +114,20 @@ def query_db(query, args=(), one=False):
 
 @app.cli.command('initdb')
 def initdb_command():
-    """Initializes the database."""
     init_db()
     print('Initialized the database.')
 
 def connect_db():
-    """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
